16d4715122c368549d225af7dc5a6339972237d7
okdshin <kokuzen@gmail.com>
2016-07-16 02:22:57 +0900

    implemented simple mnist

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import numpy as np
 import chainer
-#from chainer import cuda, Function, gradient_check, report, training, utils, Variable
-from chainer import cuda, Function, gradient_check, utils, Variable
-from chainer import datasets, optimizers, serializers
+from chainer import cuda, Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
 from chainer import Link, Chain, ChainList
 import chainer.functions as F
 import chainer.links as L
 from chainer.training import extensions
 
-def main():
-    f = L.Linear(3, 2)
-    f.zerograds()
-    x = Variable(np.array([[1,2,3], [4,5,6]], dtype=np.float32))
-    y = f(x)
-    print(y.data)
-    print(f.W.data)
-    y.grad = np.ones((2,2), dtype=np.float32)
-    y.backward()
-    print(f.W.grad)
-    print(f.b.grad)
+class MLP(Chain):
+    def __init__(self):
+        super(MLP, self).__init__(
+            l1=L.Linear(784, 100),
+            l2=L.Linear(100, 100),
+            l3=L.Linear(100, 10)
+        )
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        h2 = F.relu(self.l2(h1))
+        y = self.l3(h2)
+        return y
 
-    optimizer = optimizers.SGD()
-    optimizer.setup(f)
+def main():
     train, test = datasets.get_mnist()
-    print(len(train), len(train[0]))
-    print(train[0][1])
+    train_iter = iterators.SerialIterator(train, batch_size=100)
+    test_iter = iterators.SerialIterator(test, batch_size=100, repeat=False, shuffle=False)
+
+    model = L.Classifier(MLP())
+    optimizer = optimizers.SGD()
+    optimizer.setup(model)
+
+    updater = training.StandardUpdater(train_iter, optimizer)
+    trainer = training.Trainer(updater, (20, 'epoch'), out='result')
+    trainer.extend(extensions.Evaluator(test_iter, model))
+    trainer.extend(extensions.LogReport())
+    trainer.extend(extensions.PrintReport(['epoch', 'main/accuracy', 'validation/main/accuracy']))
+    trainer.extend(extensions.ProgressBar())
+    trainer.run()
+
 
 if __name__ == "__main__":
     main()
