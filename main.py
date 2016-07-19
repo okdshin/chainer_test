9d25aa5357bf006a8b1c7dd7d014a990de0b8dd9
okdshin <kokuzen@gmail.com>
2016-07-19 12:11:17 +0900

    implemented DeepCNet

--- main.py
+++ main.py
@@ -7,25 +7,30 @@ import chainer.functions as F
 import chainer.links as L
 from chainer.training import extensions
 
-class MLP(Chain):
-    def __init__(self):
-        super(MLP, self).__init__(
-            l1=L.Linear(784, 100),
-            l2=L.Linear(100, 100),
-            l3=L.Linear(100, 10)
-        )
+class DeepCNet(ChainList):
+    def __init__(self, l, k, output_dim):
+        cnns = []
+        cnns.append(L.Convolution2D(in_channels=3, out_channels=k,
+                                    ksize=3, pad=1, use_cudnn=True))
+        for i in range(l):
+            cnns.append(L.Convolution2D(in_channels=(i+1)*k, out_channels=(i+2)*k,
+                                        ksize=2, pad=1, use_cudnn=True))
+        fc=L.Linear((l+1)*k, output_dim)
+        super(DeepCNet, self).__init__(*cnns, fc)
+
     def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        y = self.l3(h2)
+        y = x
+        for conv in self[:-1]:
+            y = F.relu(F.max_pooling_2d(conv(y), ksize=2, stride=2, cover_all=False))
+        y = F.relu(self[-1](y))
         return y
 
 def main():
-    train, test = datasets.get_mnist()
+    train, test = datasets.get_cifar10(ndim=3)
     train_iter = iterators.SerialIterator(train, batch_size=100)
     test_iter = iterators.SerialIterator(test, batch_size=100, repeat=False, shuffle=False)
 
-    model = L.Classifier(MLP())
+    model = L.Classifier(DeepCNet(5, 300, 10))
     model.to_gpu()
     optimizer = optimizers.SGD()
     optimizer.setup(model)
